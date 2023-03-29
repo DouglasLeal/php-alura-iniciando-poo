71815a1e486335c3f0d7a5be9712971e8e97bc07
Douglas Leal <douglas.leal@live.com>
2023-03-29 10:04:26 -0300

    Conta - classe criada

--- src/Conta.php
+++ src/Conta.php
@@ -2,5 +2,34 @@
 
 class Conta
 {
-    
+    public $cpf;
+    public $titular;
+    public $saldo = 0;
+
+    public function sacar(float $valor): void
+    {
+        if($valor > $this->saldo){
+            echo "Saldo insuficiente.";
+        }else{
+            $this->saldo -= $valor;
+        }
+    }
+
+    public function depositar(float $valor): void
+    {
+        if($valor <= 0){
+            echo "Valor inválido.";
+        }else{
+            $this->saldo += $valor;
+        }
+    }
+
+    public function transferir(float $valor, Conta $destino): void{
+        if($valor > $this->saldo){
+            echo "Saldo insuficiente.";
+        }else{
+            $this->sacar($valor);
+            $destino->depositar($valor);
+        }
+    }
 }
